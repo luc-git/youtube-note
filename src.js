5f4793edef65426341b0066cbf5ed21c8ba1be77
luc-git <102831178+luc-git@users.noreply.github.com>
2024-04-10 18:28:21 +0200

    edit extension name and refactor code

--- src.js
+++ src.js
@@ -1,20 +1,32 @@
-let video = document.querySelector("video")
-let player = document.querySelector("#player")  
-let div = document.createElement("div")
-let content = document.createElement("div")
-div.id = "note"
-content.id = "content"
-player.appendChild(div)
-div.appendChild(content)
-//div.style.visibility = "hidden"
-getnote()
+main()
 
-async function getnote() {
-  const request = await fetch("https://www.dolthub.com/api/v1alpha1/luc-dolt/note/main?q=SELECT+*+FROM+%60note%60")
+async function getnote(content, video) {
+  let url = location.href.split("v=")[1]
+  console.log("https://www.dolthub.com/api/v1alpha1/luc-dolt/note/main?q=SELECT+*%0AFROM+note%0AWHERE+videoid%3D%22" + url + "%22+AND+" + video.currentTime + "+%3E%3D+timebegin+AND+" + video.currentTime + "+%3C%3D+timeend%3B%0A")
+  const request = await fetch("https://www.dolthub.com/api/v1alpha1/luc-dolt/note/main?q=SELECT+*%0AFROM+note%0AWHERE+videoid%3D%22" + url + "%22+AND+" + video.currentTime + "+%3E%3D+timebegin+AND+" + video.currentTime + "+%3C%3D+timeend%3B%0A")
   const response = await request.json()
+  if (response["rows"][0] == undefined){
+    content.innerText = ""
+    return
+  }
   content.innerText = JSON.stringify(response["rows"][0]["note"])
-  console.log(JSON.stringify(response["rows"][0]["note"]))
 }
 
-video.addEventListener("timeupdate", () => {
-})
+function main() {
+  if (!location.href.includes("watch")){
+    return
+  }
+  let video = document.querySelector("video")
+  let player = document.querySelector("#player")  
+  let div = document.createElement("div")
+  let content = document.createElement("div")
+  div.id = "note"
+  content.id = "content"
+  player.appendChild(div)
+  div.appendChild(content)
+  getnote(content, video)
+  video.addEventListener("timeupdate", () => {
+    getnote(content, video)
+  })
+  console.log("HEREEEE" + location.href)
+}
